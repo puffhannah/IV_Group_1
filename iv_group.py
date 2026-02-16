df7660902a0de0b4bae0c18d42d4b2dd08c3ffdd
Hannah <puffhannah8@gmail.com>
2026-02-16 21:02:14 +0000

    round 1

--- iv_group.py
+++ iv_group.py
@@ -3,12 +3,13 @@ import altair as alt
 from altair.datasets import data
 df= pd.read_csv("medical_clean_final.csv")
 
-chart =alt.Chart(df).mark_circle(size=60).encode(
-    x='blood_pressure',
-    y='glucose_levels',
+chart =alt.Chart(df).mark_point(size=30, opacity=0.5).encode(
+    x= alt.X('blood_pressure', scale=alt.Scale(zero= False)),
+    y= alt.Y('glucose_levels', scale= alt.Scale(zero= False)),
     color='condition',
-    shape= 'gender',
+    shape= alt.Shape('gender', scale= alt.Scale(domain=['female','male'], range=['square','circle'])),
     tooltip=['condition', 'blood_pressure', 'glucose_levels', 'gender']
 ).interactive()
 #chart.show()
 chart.save("first_vis.html")
+print(df['gender'].unique())
