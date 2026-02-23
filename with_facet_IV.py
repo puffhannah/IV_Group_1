518c7ea0e2898f090f4225cb566dbfe58ac98aea
Hannah <puffhannah8@gmail.com>
2026-02-23 23:16:16 +0000

    added smoking_status

--- with_facet_IV.py
+++ with_facet_IV.py
@@ -34,26 +34,35 @@ gender_radio= alt.binding_radio(
 select_gender= alt.selection_point(
     fields=['gender'], bind=gender_radio
 )
-chart =alt.Chart(df).mark_point(size=60, opacity=1, filled=True).transform_calculate(
-    age_group=alt.expr.if_(alt.datum.age< 30,'under 30',alt.expr.if_(alt.datum.age<60,'30-59',alt.expr.if_(alt.datum.age<80,'60-79','80+')))
+chart =alt.Chart(df).mark_point(size=80, opacity=1, filled=True).transform_calculate(
+    age_group=alt.expr.if_(alt.datum.age< 30,'Under 30',alt.expr.if_(alt.datum.age<60,'30-59',alt.expr.if_(alt.datum.age<80,'60-79','80+')))
 ).encode(
     x= alt.X('blood_pressure', scale=alt.Scale(domain= [70,200])),
     y= alt.Y('glucose_levels', scale= alt.Scale(domain=[70,200])),
-    color= alt.condition(select_condition,'condition',alt.value('black')),
+    color= alt.condition(select_condition,'condition:N',alt.value('black')),
     shape= alt.Shape('gender', scale= alt.Scale(domain=['female','male'], range=['square','circle'])),
-    tooltip=['condition', 'blood_pressure', 'glucose_levels', 'gender'],
+    size= alt.Size('smoking_status:N',scale=alt.Scale(domain=['Non-Smoker','Smoker'], range=[60,150])),
+    tooltip=['condition', 'blood_pressure', 'glucose_levels', 'gender','smoking_status'],
 ).transform_filter(select_condition
 ).transform_filter(select_gender
-).properties(width=500,height=500
+).properties(width=400,height=400
 ).facet(
-    facet= alt.Facet('age_group:N',
-    sort=['under 30','30-59','50-79','80+'],
+    facet= alt.Facet('age_group:O',
+    sort=['Under 30','30-59','50-79','80+'],
     title='Age Group'),
-    columns=4
+    columns=2,
+    title=alt.TitleParams(
+        text='Blood Pressure vs Glucose Levels by Age Group',
+        subtitle='* Elderly is defined as age 60+',  
+        subtitleColor='gray',
+        subtitleFontSize=12,
+        anchor='end')
 ).add_params(select_condition, select_gender
 ).transform_calculate(jitter = 'random()').interactive()
 chart.show()
-chart.save("first_vis_drop.html")
+chart.save("facet_vis.html")
 print(df['gender'].unique())
+
+
 
 #alt.Chart(df).mar
